0e2e6d2e1dfd24ac592688da8d0bf514c6105ad2
maxfranck <max.franck@gmail.com>
2022-05-27 21:52:27 -0300

    Indentando script

--- banco.sql
+++ banco.sql
@@ -4,12 +4,12 @@ USE `aprender_python`;
 DROP TABLE IF EXISTS `usuarios`;
 
 CREATE TABLE `usuarios` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`nome` varchar(255) NOT NULL,
-`email` varchar(255) NOT NULL,
-`telefone` char(11) DEFAULT NULL,
-`create_time` timestamp NULL DEFAULT current_timestamp(),
-PRIMARY KEY (`id`),
-UNIQUE KEY `email_UNIQUE` (`email`),
-UNIQUE KEY `CPF_UNIQUE` (`telefone`)
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `telefone` char(11) DEFAULT NULL,
+    `create_time` timestamp NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email_UNIQUE` (`email`),
+    UNIQUE KEY `CPF_UNIQUE` (`telefone`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
